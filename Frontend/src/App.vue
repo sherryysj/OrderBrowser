7830db69672573417f1e3237c2f4473ac7590c5b
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-01-23 17:14:46 +1100

    delete preset files and generate OrderBrowser component

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <OrderBrowser msg="Welcome to Order Browse Page" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OrderBrowser from "./components/OrderBrowser.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    OrderBrowser,
+  },
+};
 </script>
 
 <style>
